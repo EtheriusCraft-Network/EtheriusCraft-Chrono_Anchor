68d75127023f21854ed7306fcc7107361c5ed427
EtheriusCraft — Network <etheriusmc@gmail.com>
2025-01-27 19:02:45 -0300

    Atualização da Apresentação do Repositório

--- docs/script.js
+++ docs/script.js
@@ -10,7 +10,6 @@ let index = 0;
 document.addEventListener('DOMContentLoaded', function() {
   const blockquote = document.querySelector('.imgur-embed-pub');
 
-  // Bloqueia apenas os links de redirecionamento
   if (blockquote) {
     blockquote.querySelectorAll('a[href^="//imgur.com"]').forEach(link => {
       link.addEventListener('click', function(event) {
@@ -19,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
       });
     });
   }
-  
+});
 /*
 document.addEventListener('DOMContentLoaded', function() {
   document.querySelectorAll('.imgur-embed-pub a').forEach(link => {
